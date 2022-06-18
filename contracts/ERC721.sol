015db111f1e87bbddce77d5b2dcb4ad40c80290f
kathamas <kathamas@gmail.com>
2022-06-18 10:43:19 +0700

    max supply

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -15,6 +15,8 @@ contract MyToken is ERC721Enumerable, Pausable, AccessControl, ERC721Burnable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
+    uint256 public constant maxSupply = 1111;
+
     constructor() ERC721("MyToken", "MTK") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
@@ -35,15 +37,16 @@ contract MyToken is ERC721Enumerable, Pausable, AccessControl, ERC721Burnable {
 
     function safeMint(address to) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
+        require(tokenId < maxSupply, "Over max supply");
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        whenNotPaused
-        override(ERC721, ERC721Enumerable)
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) whenNotPaused {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
