a06ebf06f8bf8083df6a878f6293f2e241c0bfb6
Nawapat Buakoet <nawapatb.2b@hotmail.com>
2022-06-18 13:57:23 +0700

    Update ERC721 (ERC721Enumerable)
    update ERC721Enumerable
     - baseURI
     - tokenURI
     - NotRevealedURI
     - maxSupply (kathamas)
     - safeMint (kathamas)

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -7,24 +7,62 @@ import "@openzeppelin/contracts@4.6.0/security/Pausable.sol";
 import "@openzeppelin/contracts@4.6.0/access/AccessControl.sol";
 import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Strings.sol";
 
 contract MyToken is ERC721Enumerable, Pausable, AccessControl, ERC721Burnable {
+    
     using Counters for Counters.Counter;
+    using Strings for *;
 
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
+    string private baseURI;
+    string public baseExtension = ".json";
+    string public notRevealedUri;
     uint256 public constant maxSupply = 1111;
+    bool public revealed = true;
 
-    constructor() ERC721("MyToken", "MTK") {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _initBaseURI,
+        string memory _initNotRevealedUri
+    ) ERC721(_name, _symbol) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
+        setBaseURI(_initBaseURI);
+        setNotRevealedURI(_initNotRevealedUri);
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "ipfs.io/ipfs/CID/{id}.json";
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        baseURI = _newBaseURI;
+    }
+
+    function setNotRevealedURI(string memory _notRevealedURI) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        notRevealedUri = _notRevealedURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory){
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        if(revealed == false) {
+            return notRevealedUri;
+        }
+
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0 
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension)) 
+        : "";
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -34,7 +72,7 @@ contract MyToken is ERC721Enumerable, Pausable, AccessControl, ERC721Burnable {
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
-
+ 
     function safeMint(address to) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
         require(tokenId < maxSupply, "Over max supply");
