fda231edfa4c4a1f6c7994e50d7b8ba532c01a8d
Nawapat Buakoet <nawapatb.2b@hotmail.com>
2022-06-19 15:45:27 +0700

    update MintNagaKid use AccessControl

--- src/MintNagaKids.sol
+++ src/MintNagaKids.sol
@@ -3,30 +3,32 @@ pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MintNagaKids is ReentrancyGuard {
+contract MintNagaKids is AccessControl, ReentrancyGuard {
 
     IERC721Enumerable public nagaKidsContract;
     bool public isFreeMintOpen;
     bool public isWhitelistOpen;
     
     constructor(address _nagaKids) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         changeNagaKidsContract(_nagaKids);
     }
 
-    function changeNagaKidsContract(IERC721Enumerable _nagaKids) {
+    function changeNagaKidsContract(IERC721Enumerable _nagaKids) onlyRole(DEFAULT_ADMIN_ROLE) {
         nagaKidsContract = _nagaKids
     }
 
-    function changeWhitelistMintOpen(bool _isOpen) public {
+    function changeWhitelistMintOpen(bool _isOpen) public onlyRole(DEFAULT_ADMIN_ROLE) {
         isWhitelistOpen = _isOpen
     }
 
-    function changeFreeMintOpen(bool _isOpen) public {
+    function changeFreeMintOpen(bool _isOpen) public onlyRole(DEFAULT_ADMIN_ROLE) {
         isFreeMintOpen = _isOpen
     }
 
-    function whitelistMint() public nonReentrant {
+    function whitelistMint() public payable nonReentrant {
         require(isWhitelistOpen == true,"Whitelist not open to mint.");
         require(
             getTotalSupply() < 1111,
