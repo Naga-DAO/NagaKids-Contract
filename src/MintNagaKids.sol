40b5c1ba3f3cfdd8275c963f679fdaf139f9c176
Nawapat Buakoet <nawapatb.2b@hotmail.com>
2022-06-19 14:14:07 +0700

    Change MintNagaKids

--- src/MintNagaKids.sol
+++ src/MintNagaKids.sol
@@ -2,26 +2,28 @@
 pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
-interface INagaKids {
-    function totalSupply() external view returns (uint256);
-
-    function maxSupply() external view returns (uint256);
-}
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 contract MintNagaKids is ReentrancyGuard {
-    address public nagaKidsContract;
-    uint256 totalSupply;
-    uint256 maxSupply;
+
+    IERC721Enumerable public nagaKidsContract;
+    
+    constructor(address _nagaKids) {
+        changeNagaKidsContract(_nagaKids);
+    }
+
+    function changeNagaKidsContract(IERC721Enumerable _nagaKids) {
+        nagaKidsContract = _nagaKids
+    }
 
     function whitelistMint() public nonReentrant {
-        require(nagaKidsContract != address(0x0));
         require(
-            INagaKids(nagaKidsContract).totalSupply() < 1111,
+            getTotalSupply() < 1111,
             "Over supply"
-        );
-        totalSupply = INagaKids(nagaKidsContract).totalSupply();
-        maxSupply = INagaKids(nagaKidsContract).maxSupply();
+        )
+
+        
+
     }
 
     function setNagaKidsAddress(address _address) public {
@@ -29,10 +31,10 @@ contract MintNagaKids is ReentrancyGuard {
     }
 
     function getTotalSupply() public view returns (uint256) {
-        return totalSupply;
+        return nagaKidsContract.totalSupply();
     }
 
     function getMaxSupply() public view returns (uint256) {
-        return maxSupply;
+        return nagaKidsContract.maxSupply();
     }
 }
