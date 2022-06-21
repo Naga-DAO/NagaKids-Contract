99701165536c7d28c80850a0ef5e352619588cdb
Nawapat Buakoet <nawapatb.2b@hotmail.com>
2022-06-21 20:43:42 +0700

    update Role & Withdraw Function

--- contracts/MintNagaKid.sol
+++ contracts/MintNagaKid.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
@@ -8,10 +10,12 @@ import "./INagaKid.sol";
 
 contract MintNagaKid is AccessControl, ReentrancyGuard {
 
-    bytes32 public constant WHITELIST_MINT_ROUND = 0x68e7d51fdb912cb107dda2e59b053d87fcca666dd0ef5339cd3474ccb5276bba; // keccak256("WHITELIST_MINT_ROUND");
-    bytes32 public constant NAGA_HOLDER_MINT_ROUND = 0xb3c595e55271590809f54e2f4fc3a582754f45b104dd3c41666e2ad310493db3; // keccak256("NAGA_HOLDER_MINT_ROUND");
+    bytes32 public constant WHITELIST_MINT_ROUND = keccak256("WHITELIST_MINT_ROUND"); // 0x68e7d51fdb912cb107dda2e59b053d87fcca666dd0ef5339cd3474ccb5276bba
+    bytes32 public constant NAGA_HOLDER_MINT_ROUND = keccak256("NAGA_HOLDER_MINT_ROUND"); // 0xb3c595e55271590809f54e2f4fc3a582754f45b104dd3c41666e2ad310493db3
     bytes32 public constant DEFAULT = 0x0000000000000000000000000000000000000000000000000000000000000000;
     
+    bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
+
     INagaKid public nagaKidContract;
     bytes32 public currentMintRound;
     bytes32 public merkleRoot;
@@ -24,11 +28,17 @@ contract MintNagaKid is AccessControl, ReentrancyGuard {
     event NagaKidContractChanged(address nagaKidBefore, address nagaKidAfter);
     event MerkleRootChanged(bytes32 merkleRootBefore, bytes32 merkleRootAfter);
     event RoundChanged(bytes32 roundBefore, bytes32 roundAfter);
+    event Withdraw(address to, uint256 balanceOFContract , uint256 timestamp);
+    event WithdrawCurrency(address to,address currency,uint256 balanceOfContract,uint256 timestamp);
+    event WithdrawNFT(address to,address NFT,uint256 tokenId, uint256 timestamp);
 
     constructor(INagaKid _nagaKidContract, bytes32 _merkleRoot) {
+
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(WITHDRAW_ROLE, msg.sender);
         setNagaKidContract(_nagaKidContract);
         setMerkleRoot(_merkleRoot);
+
     }
 
     modifier Paused() {
@@ -72,6 +82,31 @@ contract MintNagaKid is AccessControl, ReentrancyGuard {
         nagaKidContract.batchMint(msg.sender,_amount);
 
         emit Minted(msg.sender, _amount, block.timestamp);
+    }
+
+    function withdraw(address _to) public onlyRole(WITHDRAW_ROLE) {
+        uint balanceOFContract = address(this).balance;
+        require(balanceOFContract > 0, "Insufficient Balance");
+        (bool status,) = _to.call{value: balanceOFContract }("");
+        require(status);
+
+        emit Withdraw(_to, balanceOFContract ,block.timestamp);
+    }
+
+    function withdrawCurrency(address _to,address _currency) public onlyRole(WITHDRAW_ROLE) {
+        uint balanceOfContract = IERC20(_currency).balanceOf(address(this));
+        require(balanceOfContract > 0, "Insufficient Balance");
+        IERC20(_currency).transfer(_to, balanceOfContract);
+        
+        emit WithdrawCurrency(_to,_currency,balanceOfContract,block.timestamp);
+    }
+
+    function withdrawNFT(address _to,address _NFT,uint256 _tokenId) public onlyRole(WITHDRAW_ROLE) {
+        require(IERC721(_NFT).ownerOf(_tokenId) == address(this), "This tokenId is not owned by contract");
+
+        IERC721(_NFT).safeTransferFrom(address(this), _to, _tokenId);
+        
+        emit WithdrawNFT(_to,_NFT,_tokenId,block.timestamp);
     }
 
     function isUserMinted(address _user,bytes32 _round) public view returns(bool) {
