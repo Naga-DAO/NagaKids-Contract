ced4b9025f916bbc1bebbe7b416a9131cb14d6e2
Nawapat Buakoet <nawapatb.2b@hotmail.com>
2022-06-24 22:34:44 +0700

    updated

--- contracts/ReceivedSale.sol
+++ contracts/ReceivedSale.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
-contract ReceivedSellNagaKids is PaymentSplitter {
+contract ReceivedSaleNagaKids is PaymentSplitter {
 
     constructor(address[] memory _payees, uint256[] memory _shares) PaymentSplitter(_payees ,_shares) payable {}
 
