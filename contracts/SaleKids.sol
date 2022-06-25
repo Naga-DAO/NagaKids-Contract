618b2eb919328c4eb0de58501eeb0709d93e1efb
chacree <pleum@users.noreply.github.com>
2022-06-26 01:34:49 +0700

    format code and add verify signature

--- contracts/SaleKids.sol
+++ contracts/SaleKids.sol
@@ -9,15 +9,14 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract SaleKids is Ownable, ReentrancyGuard {
-
-    bytes32 public constant WHITELIST_MINT_ROUND = keccak256("WHITELIST_MINT_ROUND"); // 0x68e7d51fdb912cb107dda2e59b053d87fcca666dd0ef5339cd3474ccb5276bba
-    bytes32 public constant NAGA_HOLDER_MINT_ROUND = keccak256("NAGA_HOLDER_MINT_ROUND"); // 0xb3c595e55271590809f54e2f4fc3a582754f45b104dd3c41666e2ad310493db3
-  
-    bytes32 public constant DEFAULT = 0x0000000000000000000000000000000000000000000000000000000000000000;
-
+    // 0x68e7d51fdb912cb107dda2e59b053d87fcca666dd0ef5339cd3474ccb5276bba
+    bytes32 public constant WHITELIST_MINT_ROUND = keccak256("WHITELIST_MINT_ROUND"); 
+    // 0xb3c595e55271590809f54e2f4fc3a582754f45b104dd3c41666e2ad310493db3
+    bytes32 public constant NAGA_HOLDER_MINT_ROUND = keccak256("NAGA_HOLDER_MINT_ROUND");
+    uint256 public constant PUBLIC_MINT_AMOUNT = 1;
 
     INagaKid public nagaKids;
-    bytes32 public currentMintRound;
+    bytes32 public currentPrivateRound;
     bytes32 public merkleRoot;
 
     bool public isPrivate = false;
@@ -34,7 +33,7 @@ contract SaleKids is Ownable, ReentrancyGuard {
     event PublicMinted(address indexed user, uint256 amount, uint256 timestamp);
     event NagaKidsChanged(address oldNagaKids, address newNagaKids);
     event MerkleRootChanged(bytes32 merkleRootBefore, bytes32 newMerkleRoot);
-    event RoundChanged(bytes32 oldRound, bytes32 newRound);
+    event PrivateRoundChanged(bytes32 oldRound, bytes32 newRound);
     event SignerChanged(address oldSigner, address afterSigner);
     event Withdraw(address to, uint256 balanceOFContract , uint256 timestamp);
     event WithdrawToken(address to,address currency, uint256 balanceOfContract, uint256 timestamp);
@@ -49,11 +48,13 @@ contract SaleKids is Ownable, ReentrancyGuard {
 
     function setPublicMint(bool _bool) public onlyOwner {
         isPublic = _bool;
+
         emit PublicMintChanged(_bool);
     }
 
     function setPrivateMint(bool _bool) public onlyOwner {
         isPublic = _bool;
+
         emit PrivateMintChanged(_bool);
     }
 
@@ -64,10 +65,9 @@ contract SaleKids is Ownable, ReentrancyGuard {
         emit NagaKidsChanged(oldNagaKids, newNagaKids);
     }
 
-    //private round
-    function setRound(bytes32 _round) public onlyOwner {
-        bytes32 _oldRound = currentMintRound;
-        currentMintRound = _round;
+    function setPrivateRound(bytes32 _round) public onlyOwner {
+        bytes32 _oldRound = currentPrivateRound;
+        currentPrivateRound = _round;
 
         emit RoundChanged(_oldRound, _round);
     }
@@ -87,15 +87,14 @@ contract SaleKids is Ownable, ReentrancyGuard {
     }
 
     function privateMint(bytes32[] calldata _proof, uint256 _amount, bytes32 _round) public payable nonReentrant {
+        // This is payable function.
+        // You can tip Naga Team if you want.
 
-        //This is payable function.
-        //You can tip Naga Team if you want.
-
-        require(isPrivate == true,"Private mint is not open.");
-        require(currentMintRound == _round, "Contract are not in this minting round.");
-        require(getTotalSupply() + _amount <= 1011,"Over Supply Amount");
+        require(isPrivate == true, "Private mint is not open.");
+        require(currentPrivateRound == _round, "Contract are not in this minting round.");
+        require(getTotalSupply() + _amount <= 1011, "Over supply amount.");
         require(isPrivateUserMinted(msg.sender, _round) == false, "You are already minted.");
-        require(MerkleProof.verify(_proof, merkleRoot, keccak256(abi.encodePacked(msg.sender, _amount, _round))), "Unauthorized WhitelistMint This User.");
+        require(MerkleProof.verify(_proof, merkleRoot, keccak256(abi.encodePacked(msg.sender, _amount, _round))), "Unauthorized whitelist mint this user.");
 
         _isPrivateUserMinted[msg.sender][_round] = true;
         _privateUserMintedAmount[msg.sender][_round] += _amount;
@@ -105,34 +104,38 @@ contract SaleKids is Ownable, ReentrancyGuard {
         emit PrivateMinted(msg.sender, _amount, block.timestamp);
     }
 
-    function publicMint(bytes calldata _sig) public payable nonReentrant {
+    function verifySignature(bytes calldata _signature, address _user) public view returns(bool) {
+        bytes32 hashMessage = keccak256(abi.encodePacked(_user, address(this)));
+        bytes32 message = ECDSA.toEthSignedMessageHash(hashMessage);
+        address receivedAddress = ECDSA.recover(message, _signature);
 
-        //This is payable function.
-        //You can tip Naga Team if you want.
+        return receivedAddress != address(0) && receivedAddress == signer;
+    }
+
+    function publicMint(bytes calldata _sig) public payable nonReentrant {
+        // This is payable function.
+        // You can tip Naga Team if you want.
         
         require(isPublic == true, "Public mint is not open.");
         require(tx.origin == msg.sender, "haha Contract can't call me");
         require(isPublicUserMinted(msg.sender) != true, "You are already minted.");
-        require(getTotalSupply() + 1 <= 1111, "Over Supply Amount");
-        require(ECDSA.recover(keccak256(abi.encodePacked(msg.sender, address(this))), _sig) == signer, "Unauthorized PublicMint This User.");
-
-        // publicMint User can get only 1 //
-        uint256 _amount = 1; 
+        require(getTotalSupply() + 1 <= 1111, "Over supply amount");
+        require(verifySignature(_sig, msg.sender), "Unauthorized public mint this user.");
 
         _isPublicUserMinted[msg.sender] = true;
-        nagaKids.safeMint(msg.sender, _amount);
-        
-        emit PublicMinted(msg.sender, _amount, block.timestamp);
 
+        nagaKids.safeMint(msg.sender, PUBLIC_MINT_AMOUNT);
+        
+        emit PublicMinted(msg.sender, PUBLIC_MINT_AMOUNT, block.timestamp);
     }
 
-    function isPublicUserMinted(address _addr) public view returns(bool){
+    function isPublicUserMinted(address _addr) public view returns(bool) {
         return _isPublicUserMinted[_addr];
     }
 
     function withdraw(address _to) public onlyOwner {
         uint balanceOFContract = address(this).balance;
-        require(balanceOFContract > 0, "Insufficient Balance");
+        require(balanceOFContract > 0, "Insufficient balance");
         (bool status,) = _to.call{value: balanceOFContract }("");
         require(status);
 
@@ -141,17 +144,17 @@ contract SaleKids is Ownable, ReentrancyGuard {
 
     function withdrawToken(address _to, address _token) public onlyOwner {
         uint balanceOfContract = IERC20(_token).balanceOf(address(this));
-        require(balanceOfContract > 0, "Insufficient Balance");
+        require(balanceOfContract > 0, "Insufficient balance");
         IERC20(_token).transfer(_to, balanceOfContract);
         
         emit WithdrawToken(_to, _token, balanceOfContract, block.timestamp);
     }
 
-    function isPrivateUserMinted(address _user,bytes32 _round) public view returns(bool) {
+    function isPrivateUserMinted(address _user, bytes32 _round) public view returns(bool) {
         return _isPrivateUserMinted[_user][_round];
     }
 
-    function privateUserMintedAmount(address _user,bytes32 _round) public view returns(uint256) {
+    function privateUserMintedAmount(address _user, bytes32 _round) public view returns(uint256) {
         return _privateUserMintedAmount[_user][_round];
     }
 
@@ -162,5 +165,4 @@ contract SaleKids is Ownable, ReentrancyGuard {
     function getMaxSupply() public view returns (uint256) {
         return nagaKids.maxSupply();
     }
-
 }
