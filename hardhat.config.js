c89630856c4f8e98371ab0a23987752873c02ecc
NuttakitDW <nuttakit@roostoo.com>
2022-06-26 10:48:26 +0700

    config optimism

--- hardhat.config.js
+++ hardhat.config.js
@@ -35,6 +35,11 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    optimism: {
+      url: process.env.OPTIMISM_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     mainnet: {
       url: process.env.MAINNET_URL || "",
       accounts:
